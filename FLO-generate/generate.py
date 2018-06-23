93bbc4fb22c94e29215e998f2f4844a0448fe0b3
Li Dinghong <lidh1@shanghaitech.edu.cn>
2018-06-23 17:08:40 +0800

    implementation of some classes

--- FLO-generate/generate.py
+++ FLO-generate/generate.py
@@ -1,39 +1,92 @@
-variables_table = []
+variables_table = {}
 
 class DependencyTree: 
 	def __init__(s):
-		self.root = "is"
+		self.root = Root("is")
+		self.subject = Expression("every student at STU")
+		self.predicative = Expression("smart")
 
 	def generate():
 		if self.subject.isUniversal():
-			outputUniversal()
+			self.subject.generateUniversal()
+			print("\\Rightarrow")
+			# self.
 
-class Sentence: 
-	def __init__(subject, verb, predicative):
-		self.subject = subject
-		self.verb = verb
-		self.predicative = predicative
+class Expression: 
+	def __init__(s):
+		self.text = s
 
-	def generate():
-		if subject.isUniversal():
-			print("\\forall")
-
-class Quantifier: 
-	def __init__(name, formula, root):
-		self.name = name
-		self.formula = formula
-		self.root = root
-
-	def getName():
-		return self.name
-
-	def lpn():
+class Universal(Expression):
+	def __init__(s):
+		self.text = s
+		self.type = "Universal"
+		self.noun = "student"
+		self.attributes = ["at STU"]
 
 	def generate():
-		noun = self.lpn()
-		variables = variables_table[noun]
-		name = self.getName()
-		output(name)
-		output(formula)
+		print("\\forall")
+		t = len(variables_table)
+		variables_table[noun] = "x_%d"%t
+		self.attributes.generate()
 
-class Term: 
+s = str(input())
+tree = DependencyTree(s)
+tree.generate()
+
+class Term(Expression): 
+	def __init__(s):
+		self.type = "Term"
+		self.text = s
+
+class Constant(Term): 
+	def generate():
+		print(self.text)
+
+class Variable(Term): 
+	def addName():
+		t = len(variables_table)
+		variables_table[self.text] = "x_%d"%t
+		return
+
+	def generate():
+		print(variables_table[self.text])
+		return
+
+class Function(Term): 
+	def __init__(s, terms):
+		self.terms = terms
+		self.type = "Term"
+		self.text = s
+
+	def generate():
+		outputSymbol(self.text)
+		print("(")
+		for term in self.terms[: -1]:
+			term.generate()
+			print(", ")
+		term.generate()
+		print(")")
+
+class Formula(Expression): 
+	# def __init__(s):
+	# 	self.type = "Formula"
+	# 	self.text = s
+	pass
+
+class Predicate(Formula): 
+	def __init__(s, terms):
+		self.text = s
+		self.terms = terms
+		self.type = "Predicate"
+
+	def generate():
+		outputSymbol(self.text)
+		print("(")
+		for term in self.terms[: -1]:
+			term.generate()
+			print(", ")
+		term.generate()
+		print(")")
+		return
+
+class Equality(Formula): 
